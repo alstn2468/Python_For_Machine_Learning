7955e187f49a0925ddef1f937bd8a978edb33f08
alstn2468 <alstn2468_@naver.com>
2018-07-24 15:51:20 +0900

    2018-07-24

---
+++ Chapter.6/numpy_lab.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def n_size_ndarray_creation(n, dtype = np.int) :
+
+def zero_or_one_or_empty_ndarray(shape, type = 0, dtype = np.int) :
+
+def change_shape_of_ndarray(X, n_row) :
+
+def concat_ndarray(X_1, X_2, axis) :
+
+def normalize_ndarray(X, axis = 99, dtype = np.float32) :
+
+def save_ndarray(X, filename = "test.npy") :
+
+def boolean_index(X, condition) :
+
+def find_nearest_value(X, target_value) :
+
+def get_n_largest_values(X, n) :
